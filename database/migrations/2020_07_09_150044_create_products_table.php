059b968343913ce894e1390d42072163b2e9c94d
XJaramillo <francisco.jaramillo01@epn.edu.ec>
2020-07-09 15:24:49 -0500

    #2 Creacion Modelo Y Migracion

---
+++ database/migrations/2020_07_09_150044_create_products_table.php
@@ -0,0 +1,45 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     *
+    - products
+    - id : bigint(20) unsigned
+    - name : varchar(255)
+    - code : varchar(80)
+    - price : double
+    - status : enum('active','deleted')
+    - created_at : timestamp
+    - updated_at : timestamp
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('name');
+            $table->string('code');
+            $table->double('price');
+            $table->enum('status',['active','deleted']);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+}
---
+++ database/migrations/2020_07_09_150223_create_customers_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCustomersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * - customers
+    - id : bigint(20) unsigned
+    - name : varchar(255)
+    - lastname : varchar(255)
+    - document : varchar(80)
+    - created_at : timestamp
+    - updated_at : timestamp
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('customers', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('name');
+            $table->string('lastname');
+            $table->string('document');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('customers');
+    }
+}
